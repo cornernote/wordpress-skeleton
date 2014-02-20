45ac9f09487826b2af3cff736bc4c1a5211fa871
cornernote <cornernote@gmail.com>
2014-02-20 15:47:19 +1030

    initial commit

---
+++ config.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Application Configuration
+ *
+ * This file should contain the production settings.
+ * To use local settings copy this file to config.local.php
+ */
+
+/**
+ * MySQL settings
+ */
+define('DB_NAME', '');
+define('DB_USER', '');
+define('DB_PASSWORD', '');
+define('DB_HOST', '');
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
+$table_prefix  = 'wp_';
+
+/**
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ */
+define('AUTH_KEY', '');
+define('SECURE_AUTH_KEY', '');
+define('LOGGED_IN_KEY', '');
+define('NONCE_KEY', '');
+define('AUTH_SALT', '');
+define('SECURE_AUTH_SALT', '');
+define('LOGGED_IN_SALT', '');
+define('NONCE_SALT', '');
+
+/**
+ * WordPress Localized Language, defaults to English.
+ *
+ * Change this to localize WordPress. A corresponding MO file for the chosen
+ * language must be installed to wp-content/languages. For example, install
+ * de_DE.mo to wp-content/languages and set WPLANG to 'de_DE' to enable German
+ * language support.
+ */
+define('WPLANG', '');
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ */
+define('WP_DEBUG', false);
+
+/**
+ * WordPress content paths.
+ */
+define('WP_CONTENT_DIR', dirname(__DIR__) . '/wp-content');
+define('WP_CONTENT_URL', 'http' . (!empty($_SERVER['HTTPS']) ? 's' : '') . '://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['SCRIPT_NAME']) . '/wp-content');
---
+++ wp-config.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * WordPress Configuration
+ *
+ * Do not modify this file.
+ * Instead use config.php or config.local.php
+ */
+ 
+/** Composer autoloader */
+require_once(__DIR__ . '/vendor/autoload.php');
+
+/** Application configuration */
+if (file_exists(__DIR__ . '/config.local.php'))
+    require_once(__DIR__ . '/config.local.php');
+else
+    require_once(__DIR__ . '/config.php');
+
+/** WordPress settings */
+require_once(ABSPATH . 'wp-settings.php');
